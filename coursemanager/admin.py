0d5c0278309bc61daad0fa549015725a015cdf77
Steve Boyle <boylesa@gmail.com>
2019-02-06 17:34:46 +0000

    Closes #9:

--- coursemanager/admin.py
+++ coursemanager/admin.py
@@ -1,9 +1,19 @@
+from django import forms
 from django.contrib import admin
 from coursemanager.models import Course, Venue, Presentation, Attendee, Trainer, CostCode, Environment, CourseMaterial, Report
 
 class CourseMaterialInline(admin.StackedInline):
     model = CourseMaterial
     extra = 1
+
+COURSES = [(course.id, course.title) for course in Course.objects.all()]
+COURSES.insert(0, ('', '----'))
+
+VENUES  = [(venue.id, venue.address) for venue in Venue.objects.all()]
+VENUES.insert(0, ('', '----'))
+
+TRAINERS  = [(trainer.id, f"{trainer.firstname} {trainer.lastname}") for trainer in Trainer.objects.all()]
+TRAINERS.insert(0, ('', '----'))
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title','duration','cost','min_attendees', 'max_attendees')
@@ -16,17 +26,29 @@ class VenueAdmin(admin.ModelAdmin):
 class AttendeeInline(admin.StackedInline):
     model = Attendee
     extra = 1
-    
+
+######
+# Presentations
+###
+class PresentationAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PresentationAdminForm, self).__init__(*args, **kwargs)
+        self.fields['course'].choices=COURSES
+        self.fields['trainer'].choices = TRAINERS
+        self.fields['venue'].choices = VENUES
+
+
 class PresentationAdmin(admin.ModelAdmin):
     list_display = ('course', 'startdate', 'starttime', 'status', 'venue', 'trainer', 'num_attendees')
     list_filter = ['startdate', 'status']
-    
-    inlines = [AttendeeInline]
-    
+
+    form = PresentationAdminForm
+    # inlines = [AttendeeInline]
+
 class AttendeeAdmin(admin.ModelAdmin):
     list_display = ('emailaddress', 'presentation', 'firstname', 'lastname', 'extension', 'attendancestatus')
     search_fields = ('emailaddress', 'firstname', 'lastname')
- 
+
 class TrainerAdmin(admin.ModelAdmin):
     list_display = ('emailaddress', 'firstname', 'lastname','extension')
     
